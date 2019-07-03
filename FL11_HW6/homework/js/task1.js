e02307cf534a541ef233bc490ff44eeb25b5b78f
Petro Fedorenko <petewaitforletters@gmail.com>
2019-07-03 13:47:35 +0300

    done homework 6

--- FL11_HW6/homework/js/task1.js
+++ FL11_HW6/homework/js/task1.js
@@ -1,1 +1,15 @@
-// Your code goes here
+const half = 2;
+let a1 = Number(prompt('Write the value of a1', 0)),
+    a2 = Number(prompt('Write the value of a2', 0)),
+    b1 = Number(prompt('Write the value of b1', 0)),
+    b2 = Number(prompt('Write the value of b2', 0)),
+    c1 = Number(prompt('Write the value of c1', 0)),
+    c2 = Number(prompt('Write the value of c2', 0)),
+    midpoint1 = (a1 + b1) / half,
+    midpoint2 = (a2 + b2) / half;
+
+if (c1 === midpoint1 && c2 === midpoint2) {
+    console.log(true);
+} else {
+    console.log(false);
+}
--- FL11_HW6/homework/js/task2.js
+++ FL11_HW6/homework/js/task2.js
@@ -1,1 +1,13 @@
-// Your code goes here
+let a = Number(prompt('Write the value of a', 0)),
+    b = Number(prompt('Write the value of b', 0)),
+    c = Number(prompt('Write the value of c', 0));
+
+if (a + b < c || a + c < b || b + c < a) {
+    console.log('Triangle doesn’t exist');
+} else if (a === b && a === c && b === c) {
+    console.log('Equivalent triangle');
+} else if (a === b || a === c || b === c) {
+    console.log('Isosceles triangle');
+} else {
+    console.log('Normal triangle');
+}
