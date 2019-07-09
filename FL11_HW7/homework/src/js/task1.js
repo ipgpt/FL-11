b8172354c9a305fccab5303d9e4a405c58b97381
Petro Fedorenko <petewaitforletters@gmail.com>
2019-07-09 11:42:05 +0300

    done homework 7

--- FL11_HW7/homework/src/js/task1.js
+++ FL11_HW7/homework/src/js/task1.js
@@ -1,1 +1,92 @@
-// Your code goes here
+const MIN_USER_LENGTH = 6,
+    MIN_PASSWORD_LENGTH = 5;
+
+let userEmail = prompt('Write your email:'),
+    userPassword, changePassword,
+    checkPassword, correct;
+
+for (let i = 1; i > 0; i--) {
+
+    if (!userEmail) {
+        alert('Canceled');
+        break;
+    } else if (userEmail.length < MIN_USER_LENGTH) {
+        alert(`I don't know any emails having name length less than 6 symbols.`);
+        break;
+    } else if (userEmail === 'user@gmail.com' ||
+        userEmail === 'admin@gmail.com') {
+        userPassword = prompt('Write your password:');
+    } else {
+        alert(`I don’t know you`);
+        break;
+    }
+
+    if (!userPassword) {
+        alert('Canceled');
+        break;
+    } else if (userEmail === 'user@gmail.com' &&
+        userPassword === 'UserPass' ||
+        userEmail === 'admin@gmail.com' &&
+        userPassword === 'AdminPass') {
+        changePassword = confirm('Do you want to change your password?');
+    } else {
+        alert('Wrong password');
+        break;
+    }
+
+    if (!changePassword) {
+        alert('You have failed the change.');
+        break;
+    }
+
+    checkPassword = prompt('Write your old password:');
+
+    if (!checkPassword) {
+        alert('Canceled');
+        break;
+    } else if (userEmail === 'user@gmail.com' &&
+        checkPassword === 'UserPass' ||
+        userEmail === 'admin@gmail.com' &&
+        checkPassword === 'AdminPass') {
+        correct = true;
+    } else {
+        alert('Wrong password');
+        break;
+    }
+
+    do {
+        userPassword = prompt('Write your new password:');
+
+        if (!userPassword) {
+            break;
+        } else if (userPassword.length < MIN_PASSWORD_LENGTH) {
+            alert(`It’s too short password. Sorry.`);
+            continue;
+        } else {
+            break;
+        }
+
+    } while (correct);
+
+    if (!userPassword) {
+        alert('Canceled');
+        break;
+    }
+
+    do {
+        checkPassword = prompt('Write your new password again:');
+
+        if (checkPassword === userPassword) {
+            alert('You have successfully changed your password.');
+            break;
+        } else {
+            alert('You wrote the wrong password');
+        }
+    } while (checkPassword);
+
+    if (!checkPassword) {
+        alert('Canceled');
+        break;
+    }
+
+}
--- FL11_HW7/homework/src/js/task2.js
+++ FL11_HW7/homework/src/js/task2.js
@@ -1,1 +1,87 @@
-// Your code goes here
+const STARTING_MESSAGE = 'Do you want to play a game?',
+    CANCEL_MESSAGE = 'You did not become a billionaire, but can.',
+    RISE_NUMBERS_RANGE = 4,
+    RISE_PRIZE = 2,
+    SECOND_PRIZE = 50,
+    THIRD_PRIZE = 25,
+    FIRST_ATTEMPT = 3,
+    ONE = 1,
+    ZERO = 0;
+
+let answer = confirm(STARTING_MESSAGE),
+    answerStatus = answer ? answer : CANCEL_MESSAGE,
+    gameStatus, restartStatus;
+
+if (answerStatus !== CANCEL_MESSAGE) {
+
+    do {
+
+        let maxSecretNumber = 9,
+            prizeQuality = ONE,
+            totalPrizeValue = ZERO;
+
+        restartStatus = '';
+
+        do {
+            let attempts = 3,
+                prizeValue = 100;
+
+            gameStatus = '';
+
+            for (; attempts >= ZERO; attempts--) {
+
+                if (attempts === ZERO) {
+                    totalPrizeValue = 0;
+                    break;
+                }
+                
+                if (attempts === FIRST_ATTEMPT) {
+                    prizeValue *= prizeQuality;
+                }
+
+                let secretNumber = Math.floor(Math.random() * maxSecretNumber),
+                    briefingMessage = `Choose a roulette pocket number from 0 to ${maxSecretNumber - ONE}
+Attempts left: ${attempts}
+Total prize: ${totalPrizeValue}$
+Possible prize on current attempt: ${prizeValue}$`,
+                    userGuessNumber = prompt(briefingMessage);
+
+                if (!(userGuessNumber === '0') && !userGuessNumber) {
+                    continue;
+                }
+
+                userGuessNumber = Number(userGuessNumber);
+
+                if (secretNumber === userGuessNumber) {
+                    totalPrizeValue += prizeValue;
+
+                    let winMessage = `Congratulation, you won!
+Your prize is: ${totalPrizeValue}$.
+Do you want to continue?`;
+
+                    gameStatus = confirm(winMessage);
+                    break;
+                } else if (attempts === FIRST_ATTEMPT) {
+                    prizeValue -= SECOND_PRIZE * prizeQuality;
+                } else {
+                    prizeValue -= THIRD_PRIZE * prizeQuality;
+                }
+            }
+            maxSecretNumber += RISE_NUMBERS_RANGE;
+
+            if (prizeQuality === ONE) {
+                prizeQuality++;
+            } else {
+                prizeQuality += RISE_PRIZE
+            }
+        } while (gameStatus);
+
+        let endMessage = `Thank you for your participation.
+Your prize is: ${totalPrizeValue}$`;
+
+        alert(endMessage);
+        restartStatus = confirm('Do you want to play again?');
+    } while (restartStatus);
+} else {
+    alert(answerStatus);
+}
