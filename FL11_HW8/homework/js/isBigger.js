d6fa3a4ea70390a25a8000a096bff1bbff24da83
Petro Fedorenko <petewaitforletters@gmail.com>
2019-07-11 11:00:08 +0300

    done 1,2,4 tasks

---
+++ FL11_HW8/homework/js/isBigger.js
@@ -0,0 +1,5 @@
+function isBigger(firstNumber, secondNumber) {
+    return firstNumber > secondNumber;
+} 
+
+isBigger(5, -1);
---
+++ FL11_HW8/homework/js/isSmaller.js
@@ -0,0 +1,5 @@
+function isSmaller(firstNumber, secondNumber) {
+    return firstNumber < secondNumber;
+}
+
+isSmaller(5 -1);
