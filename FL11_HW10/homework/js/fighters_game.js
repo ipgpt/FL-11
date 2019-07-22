f193dc0494b17a35a12466ce1873cf0dadb13b95
Petro Fedorenko <petewaitforletters@gmail.com>
2019-07-22 19:34:07 +0300

    HW10 done as class

--- FL11_HW10/homework/js/fighters_game.js
+++ FL11_HW10/homework/js/fighters_game.js
@@ -1,73 +1,91 @@
-function Fighter(inputObject) {   
-    this.data = {
-        name: inputObject.name,
-        damage: inputObject.damage,
-        hp: inputObject.hp,
-        agility: inputObject.agility,
-        wins: 0,
-        loss: 0
-    };
-    this.getName = function () {
-        return this.data.name;
-    };
-    this.getDamage = function () {
-        return this.data.damage;
-    };
-    this.getHealth = function () {
-        return this.data.hp;
-    };
-    this.getAgility = function () {
-        return this.data.agility;
-    };
-    this.attack = function (defender) {
-        const rangeChance = 100;
-        let defChance = defender.data.agility;
-        let attackValue = Math.floor(Math.random() * rangeChance + 1);
-
-        if (attackValue > defChance) {
-            defender.dealDamage(this.data.damage);
-            console.log(`${this.data.name} makes ${this.data.damage} damage to ${defender.data.name}`);
+const privateData = new WeakMap();
+class Fighter {
+    constructor(inputObject) {
+        privateData.set(this, {
+            name: inputObject.name,
+            damage: inputObject.damage,
+            hp: inputObject.hp,
+            agility: inputObject.agility,
+            wins: 0,
+            loss: 0
+        });
+    }
+    getName() {
+        return privateData.get(this).name;
+    }
+    getDamage() {
+        return privateData.get(this).damage;
+    }
+    getHealth() {
+        return privateData.get(this).hp;
+    }
+    getAgility() {
+        return privateData.get(this).agility;
+    }
+    addWins() {
+        privateData.get(this).wins += 1;
+    }
+    addLoss() {
+        privateData.get(this).loss += 1;
+    }
+    logCombatHistory() {
+        console.log(`Name: ${this.getName()}` +
+            `Wins: ${privateData.get(this).wins}, Losses: ${privateData.get(this).loss}`);
+    }
+    heal(num) {
+        privateData.get(this).hp += num;
+    }
+    dealDamage(num) {
+        privateData.get(this).hp -= num;
+        if (privateData.get(this).hp < 0) {
+            privateData.get(this).hp = 0;
+        }
+    }
+    attack(defender) {
+        const attackRange = 100;
+        let dodge = defender.getAgility();
+        let attackChance = Math.floor(Math.random() * attackRange + 1);
+        if (attackChance > dodge) {
+            defender.dealDamage(this.getDamage());
+            console.log(`${this.getName()} makes ${this.getDamage()} damage to ${defender.getName()}`);
         } else {
-            console.log(`${this.data.name}'s attack missed`);
+            console.log(`${this.getName()}'s attack missed`);
         }
-    };
-    this.logCombatHistory = function () {
-        console.log(`Name: ${this.data.name}, Wins: ${this.data.wins}, Losses: ${this.data.loss}`);
-    };
-    this.heal = function (num) {
-        this.data.hp += num;
-    };
-    this.dealDamage = function (num) {
-        this.data.hp -= num;
-        if (this.data.hp < 0) {
-            this.data.hp = 0;
-        }
-    };
-    this.addWins = function () {
-        this.data.wins += 1;
-    };
-    this.addLoss = function () {
-        this.data.loss += 1;
-    };
+    }
 }
 
 function battle(firstFighter, secondFighter) {
-    if (secondFighter.data.hp === 0) {
+    if (secondFighter.getHealth() === 0) {
         return console.log(`${secondFighter.getName()} is dead and can't fight`);
-    } else if (firstFighter.data.hp === 0) {
+    } else if (firstFighter.getHealth() === 0) {
         return console.log(`${firstFighter.getName()} is dead and can't fight`);
     }
     firstFighter.attack(secondFighter);
-    if (secondFighter.data.hp === 0) {
+    if (secondFighter.getHealth() === 0) {
         secondFighter.addLoss();
         firstFighter.addWins();
         return console.log(`${secondFighter.getName()} is dead and can't fight`);
     }
     secondFighter.attack(firstFighter);
-    if (firstFighter.data.hp === 0) {
+    if (firstFighter.getHealth() === 0) {
         firstFighter.addLoss();
         secondFighter.addWins();
         return console.log(`${firstFighter.getName()} is dead and can't fight`);
     }
     return battle(firstFighter, secondFighter);
 }
+
+const troll = new Fighter({
+        name: 'Troll',
+        damage: 20,
+        hp: 100,
+        agility: 25
+    }),
+    goblin = new Fighter({
+        name: 'Goblin',
+        damage: 30,
+        hp: 80,
+        agility: 40
+    });
+
+battle(troll, goblin);
