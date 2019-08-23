741882d5a04287e6f21768108bf313b752395ec0
Petro Fedorenko <petewaitforletters@gmail.com>
2019-08-23 21:00:37 +0300

    HW17 done task 7 and some fix

--- FL_11_17_homework_es_next/homework/js/tasks.js
+++ FL_11_17_homework_es_next/homework/js/tasks.js
@@ -4,13 +4,24 @@ const maxElement = arr => Math.max(...arr),
     copyArray = arr => [...arr],
     addUniqueId = obj => Object.assign({}, {[id]: 'symbolized'}, obj),
 
-    findUniqueElements = function(arr) {
-        let resultSet = new Set();
-        arr.forEach(function(el) {
-          if (!resultSet.has(el)) {
-            resultSet.add(el);
-          }
-        });
+    findUniqueElements = function (arr) {
+        let resultSet = new Set(arr);
         return [...resultSet];
-      },
+    },
     hideNumber = str => '*'.repeat(str.length - 4) + str.slice(-4),
+    add = function (...numbers) {
+        let [a, b] = numbers,
+        sum = a + b;
+        function required(result) {
+            try {
+                if (isNaN(result)) {
+                    throw 'Error';
+                } else {
+                    return result;
+                }
+            } catch {
+                throw new Error('Missing property');
+            }
+        }
+        return required(sum);
+    };
