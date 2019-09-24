e2d2a6695b1b22428fa44a0c6cf92bce663d9268
Petro Fedorenko <petro_fedorenko@epam.com>
2019-09-24 16:01:41 +0300

    HW26 done task2 subtask1

--- FL11_HW26/homework/task2/js/index.js
+++ FL11_HW26/homework/task2/js/index.js
@@ -1,6 +1,7 @@
 const $list = $(".list");
 const $input = $("#add-input");
 const $add = $("#add-submit");
+const $form = $("form");
 
 const todos = [
   {
@@ -12,3 +13,38 @@ const todos = [
     done: true
   }
 ];
+
+$form.submit(function(e) {
+  e.preventDefault();
+});
+
+$add.click(function() {
+  let li = $("<li>").addClass("item"),
+    span = $("<span>").addClass("item-text"),
+    button = $("<button>")
+      .addClass("item-remove")
+      .text("Remove");
+  if ($input.val()) {
+    span.text($input.val());
+    $list
+      .hide()
+      .append(li.append(span).append(button))
+      .fadeIn(600);
+  } else {
+    alert("Warning: Write the task!");
+  }
+});
+
+$(document).ready(function() {
+  $(document).on("click", ".item-text", function() {
+    $(this).toggleClass("done");
+  });
+
+  $(document).on("click", ".item-remove", function() {
+    $(this)
+      .parent()
+      .fadeOut(400, function() {
+        $(this).remove();
+      });
+  });
+});
