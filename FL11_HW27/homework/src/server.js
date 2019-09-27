4d62fb3f57303ca5e6112b5164ab1253bb997779
Petro Fedorenko <petewaitforletters@gmail.com>
2019-09-27 15:32:52 +0300

    add server code

--- FL11_HW27/homework/src/server.js
+++ FL11_HW27/homework/src/server.js
@@ -1,1 +1,18 @@
-// Your code goes here
+const express = require("express");
+const app = express();
+const port = 3000;
+
+let deleteAutorization = require("./middlewares/delete-authorization");
+app.use(deleteAutorization);
+
+app.get("/", (request, response) => {
+ response.send("Hello from Express!");
+});
+
+app.listen(port, err => {
+ if (err) {
+ return console.log("something bad happened", err);
+ }
+ console.log(`server is listening on ${port}`);
+});
+
