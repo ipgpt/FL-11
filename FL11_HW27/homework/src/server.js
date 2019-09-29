583d99d7195f24c480c1ca7f6a1b43bf13c30d01
Petro Fedorenko <petewaitforletters@gmail.com>
2019-09-29 21:06:29 +0300

    HW27 add get '/car/' and 'car/id/'

--- FL11_HW27/homework/src/server.js
+++ FL11_HW27/homework/src/server.js
@@ -2,12 +2,26 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-let deleteAutorization = require("./middlewares/delete-authorization");
-app.use(deleteAutorization);
+const dataJSON = require("../db/data.json");
+//let deleteAutorization = require("./middlewares/delete-authorization");
+//app.use(deleteAutorization);
 
 app.get("/", (request, response) => {
  response.send("Hello from Express!");
 });
+
+app.get("/car/", (request, response) => {
+  response.status(200).send(dataJSON);
+ });
+
+ app.get("/car/:id", (request, response) => {
+  const carId = dataJSON.find(carData => carData.id === parseInt(request.params.id));
+  if (carId) {
+    response.status(200).send(carId);
+  } else {
+    response.status(404).send('id not found');
+  }
+ });
 
 app.listen(port, err => {
  if (err) {
